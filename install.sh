c2b86c392db570d8b0e96a06b793e6a577ffe01b
Alexander Herbert <73660072+Blockland-crpto@users.noreply.github.com>
2021-09-16 13:22:32 +0000

    changed install.sh (again)

--- install.sh
+++ install.sh
@@ -10,4 +10,6 @@ cp /home/alexh-admin/Ice-Search/index.html /var/www/html/index.html
 cp /home/alexh-admin/Ice-Search/style.css /var/www/html/style.css
 cp /home/alexh-admin/Ice-Search/about.html /var/www/html/about.html
 
+rm -rf /home/alexh-admin/Ice-Search
+
 echo Done!
