604cdb5245043789d307c3556ff47cdb40aee378
Alexander Herbert <73660072+Blockland-crpto@users.noreply.github.com>
2021-09-16 13:21:04 +0000

    changed install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 sudo su
 
-git clone
+git clone https://github.com/Blocky-studio/Ice-Search
 
 rm -rf /var/www/html/index.html
 rm -rf /var/www/html/style.css
 rm -rf /var/www/html/about.html
 
-cp /home/alexh-admin/index.html /var/www/html/index.html
-cp /home/alexh-admin/style.css /var/www/html/style.css
-cp /home/alexh-admin/about.html /var/www/html/about.html
+cp /home/alexh-admin/Ice-Search/index.html /var/www/html/index.html
+cp /home/alexh-admin/Ice-Search/style.css /var/www/html/style.css
+cp /home/alexh-admin/Ice-Search/about.html /var/www/html/about.html
 
 echo Done!
